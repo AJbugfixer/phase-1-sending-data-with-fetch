64404a0da103064792ece2c4aef73e1b7f981180
Ajay <ajay.ip3105@gmail.com>
2022-05-07 03:37:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    const labData ={
+        name: `${name}`,
+        email: `${email}`,
+    }
+
+    const labDataObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(labData),
+    };
+
+    return fetch("http://localhost:3000/users", labDataObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object){
+            document.body.innerHTML = object.id;
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message;
+        })
+};
+
+submitData()
